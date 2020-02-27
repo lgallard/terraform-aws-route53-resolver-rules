d9534f21c378711c34c65c2b04c58f7e6a729d9f
Luis M. Gallardo D <lgallard@gmail.com>
2020-02-27 18:21:21 -0300

    Fix rule associations

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_route53_resolver_rule" "r" {
   count = length(var.rules)
 
   rule_type            = "FORWARD"
-  resolver_endpoint_id = var.resolver_endpoint
+  resolver_endpoint_id = var.resolver_endpoint_id
 
   domain_name = lookup(element(var.rules, count.index), "domain_name", null)
   name        = lookup(element(var.rules, count.index), "rule_name", null)
@@ -12,7 +12,7 @@ resource "aws_route53_resolver_rule" "r" {
     for_each = lookup(element(var.rules, count.index), "ips", [])
     content {
       ip   = split(":", target_ip.value)[0]
-      port = length(split(":", split(":", target_ip.value))) == 1 ? 53 : split(":", target_ip.value)[1]
+      port = length( split(":", target_ip.value)) == 1 ? 53 : split(":", target_ip.value)[1]
     }
   }
 
