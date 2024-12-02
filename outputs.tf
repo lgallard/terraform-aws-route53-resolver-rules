f65c67dd40d9408eb2d812ec6c113230a70e50b9
Denis Boulas <dene14@gmail.com>
2024-12-02 21:15:19 +0300

    Updated module

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "rr_ids" {
   description = "Resolver rule IDs"
   value       = try(aws_route53_resolver_rule.r.*.id, [])
 }
+
+output "resolver_rules" {
+  value = {
+    for rule in aws_route53_resolver_rule.r : rule.domain_name => {
+      name = rule.name
+      type = rule.rule_type
+      ttl  = rule.resolver_endpoint_id
+    }
+  }
+}
