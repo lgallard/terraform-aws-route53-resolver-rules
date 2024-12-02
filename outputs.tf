22659bab1a6efe6eeaf361a93a3d151dbea6c07b
Denis Boulas <dene14@gmail.com>
2024-12-02 21:28:42 +0300

    Updated module

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,10 @@
 output "resolver_rules" {
   value = {
     for rule in aws_route53_resolver_rule.r : rule.domain_name => {
-      name                 = rule.name
-      rule_type            = rule.rule_type
-      esolver_endpoint_id  = rule.resolver_endpoint_id
+      name                  = rule.name
+      rule_type             = rule.rule_type
+      resolver_rule_id      = rule.id
+      resolver_endpoint_id  = rule.resolver_endpoint_id
     }
   }
 }
