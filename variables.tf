5b350a947a4adab39ba9c9cf753a714dbc1f86f0
Luis M. Gallardo D. <lgallard@gmail.com>
2025-03-28 01:59:11 +0100

    fix: update variable descriptions and required Terraform version (#20)
    - Enhanced description for resolver_endpoint_id variable for clarity.
    - Updated tags variable description to specify key-value pair format.
    - Changed required Terraform version from 0.12 to 1.0.

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "resolver_endpoint_id" {
-  description = "The ID of the outbound resolver endpoint that you want to use to route DNS queries to the IP addresses that you specify using target_ip."
+  description = "The ID of the outbound resolver endpoint that you want to use to route DNS queries to the IP addresses specified in target_ip"
   type        = string
+  default     = null
 }
 
 variable "rules" {
@@ -10,7 +11,7 @@ variable "rules" {
 }
 
 variable "tags" {
-  default     = {}
-  description = "Map of tags to apply to supported resources"
+  description = "Map of tags to apply to supported resources. Each tag is a key-value pair stored as a map of strings."
   type        = map(string)
+  default     = {}
 }
